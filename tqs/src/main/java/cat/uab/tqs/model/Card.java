fec2fb71a39d4de945da33fbc5f30027dde907d7
arnaausole <1708094@uab.cat>
2025-11-12 09:19:07 +0100

    Contstructor + Getters tests created in Card class

--- tqs/src/main/java/cat/uab/tqs/model/Card.java
+++ tqs/src/main/java/cat/uab/tqs/model/Card.java
@@ -1,5 +1,18 @@
 package cat.uab.tqs.model;
 
-public class Card {
-    
-}
+public class Card 
+{
+    private String suit;
+    private String rank;
+
+    public Card(String suit, String rank) {
+        
+    }
+
+    public String getSuit() {
+        
+    }
+
+    public String getRank() {
+        
+    }
--- tqs/src/test/java/cat/uab/tqs/model/CardTest.java
+++ tqs/src/test/java/cat/uab/tqs/model/CardTest.java
@@ -1,7 +1,17 @@
 package cat.uab.tqs.model;
 
-public class CardTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    
-    
+
+class CardTest {
+
+    @Test
+    void testCreateCardAndGetValues() 
+    {
+        Card card = new Card("Hearts", "A");
+        assertEquals("Hearts", card.getSuit());
+        assertEquals("A", card.getRank());
+    }
+
 }
