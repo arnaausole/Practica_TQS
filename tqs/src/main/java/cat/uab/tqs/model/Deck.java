56a686cff79123c9bbab42b925b91f14fc95f87a
arnaausole <1708094@uab.cat>
2025-11-12 10:03:25 +0100

    Refactor Deck constructor + getcaerds + test passed

--- tqs/src/main/java/cat/uab/tqs/model/Deck.java
+++ tqs/src/main/java/cat/uab/tqs/model/Deck.java
@@ -4,15 +4,26 @@ import java.util.ArrayList;
 
 public class Deck {
 
-    public Deck()
-    {
+    private ArrayList<Card> cards;
 
+    public Deck() {
+        
+        cards = new ArrayList<>();
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
 
+        for (String suit : suits) 
+        {
+            for (String rank : ranks) 
+            {
+                cards.add(new Card(suit, rank));
+            }
+        }
     }
 
-    public ArrayList<Card> getCards() {
-
-
+    public ArrayList<Card> getCards() 
+    {
+        return cards;
     }
     
 }
