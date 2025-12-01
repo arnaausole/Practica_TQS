48675453f351ce40a9aabf3b9b7e7a5991ec3005
arnaausole <1708094@uab.cat>
2025-12-01 17:17:19 +0100

    minimum Hand methods done

--- tqs/src/main/java/cat/uab/tqs/model/Hand.java
+++ tqs/src/main/java/cat/uab/tqs/model/Hand.java
@@ -1,9 +1,29 @@
 package cat.uab.tqs.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Hand {
+
     private List<Card> cards = new ArrayList<>();
 
-    
+    public void addCard(Card c) {
+        cards.add(c);
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public int getValue() {
+        int total = 0;
+        for (Card c : cards) {
+            total += c.getNumericValue();
+        }
+        return total;
+    }
+
+    public boolean isBust() {
+        return getValue() > 21;
+    }
 }
--- tqs/src/test/java/cat/uab/tqs/model/HandTest.java
+++ tqs/src/test/java/cat/uab/tqs/model/HandTest.java
@@ -3,7 +3,6 @@ package cat.uab.tqs.model;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 
 public class HandTest {
 
