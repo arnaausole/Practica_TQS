ad923b1bc40aa7c7802cbde6dd3e3ed0ae6ab04d
davidal15 <dalopez1503@gmail.com>
2025-12-02 20:05:10 +0100

    Update ShuffleRandom to implement Shuffler interface

--- tqs/src/main/java/cat/uab/tqs/model/ShuffleRandom.java
+++ tqs/src/main/java/cat/uab/tqs/model/ShuffleRandom.java
@@ -3,8 +3,9 @@ package cat.uab.tqs.model;
 import java.util.Collections;
 import java.util.List;
 
-public class ShuffleRandom implements MockShuffleRandom {
+public class ShuffleRandom implements Shuffler {
     @Override
     public void shuffle(List<Card> cards) {
         Collections.shuffle(cards);
 }
+
