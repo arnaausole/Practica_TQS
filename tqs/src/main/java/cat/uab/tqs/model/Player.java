611fa8e6ead18a64c47f80c909cf846267bc0678
arnaausole <1708094@uab.cat>
2025-12-02 12:14:53 +0100

    Minimum version Player done

--- tqs/src/main/java/cat/uab/tqs/model/Player.java
+++ tqs/src/main/java/cat/uab/tqs/model/Player.java
@@ -6,25 +6,30 @@ public class Player {
     private boolean standing;
 
     public Player() {
-        
+        hand = new Hand();
+        standing = false;
     }
 
     public Hand getHand() {
-
+        return hand;
     }
 
     public boolean isStanding() {
-
+        return standing;
     }
 
     public void hit(Card c) {
-        
+        if (!standing) {
+            hand.addCard(c);
+        }
     }
 
     public void stand() {
+        standing = true;
     }
 
     public void reset() {
-        
+        hand = new Hand();
+        standing = false;
     }
 }
