d908a8488039cd51bf8b81ee035173c77654ae4a
davidal15 <dalopez1503@gmail.com>
2025-12-02 20:08:38 +0100

    MockShuffler fet v2

--- tqs/src/main/java/cat/uab/tqs/mocks/MockShuffleRandom.java
+++ tqs/src/main/java/cat/uab/tqs/mocks/MockShuffleRandom.java
@@ -2,6 +2,19 @@ package cat.uab.tqs.mocks;
 
 import cat.uab.tqs.model.*;
 
-public interface MockShuffleRandom {
-    public void shuffle(List<Card> cards);
+public interface MockShuffleRandom implements Shuffler {
+
+    List<Card> predefinedOrder = List.of(
+        new Card("Hearts", "A"),
+        new Card("Diamonds", "K"),
+        new Card("Clubs", "Q"),
+        new Card("Spades", "J")
+        // Add more cards as needed for testing
+    );
+
+    @Override
+    public void shuffle(List<Card> cards) {
+        cards.clear();
+        cards.addAll(predefinedOrder);
+    }
 }
