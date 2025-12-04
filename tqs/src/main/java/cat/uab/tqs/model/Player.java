f77008de3a9ff0a61d735205463047edfaca14d1
arnaausole <1708094@uab.cat>
2025-12-04 10:30:40 +0100

    invariant + pre/postcond player

--- tqs/src/main/java/cat/uab/tqs/model/Player.java
+++ tqs/src/main/java/cat/uab/tqs/model/Player.java
@@ -5,31 +5,61 @@ public class Player {
     private Hand hand; // baralla
     private boolean standing; // es planta (no agafa mes cartes)
 
+    private boolean invariant() {
+        return hand != null;
+    }
+
     public Player() {
         hand = new Hand();
         standing = false;
+
+        assert invariant();
+        assert !standing;
     }
 
     public Hand getHand() {
+        assert invariant();
         return hand;
     }
 
     public boolean isStanding() {
+        assert invariant();
         return standing;
     }
 
     public void hit(Card c) {
+        assert invariant();
+        //precond
+        assert c != null;
+
         if (!standing) {
             hand.addCard(c);
         }
+
+        assert invariant();
     }
 
     public void stand() {
+
+        assert invariant();
+
         standing = true;
+
+        assert invariant();
     }
 
     public void reset() {
+
+        assert invariant();
+        
         hand = new Hand();
         standing = false;
+
+        assert invariant();
+
+        //postcond
+        assert hand.getCards().isEmpty();
+        assert !standing;
+
     }
 }
