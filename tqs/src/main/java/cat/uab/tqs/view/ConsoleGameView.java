224fcd0c9131338e1e219d13b28d596c6f59fce7
arnaausole <1708094@uab.cat>
2025-12-04 11:30:35 +0100

    modifiquem consolegameview

--- tqs/src/main/java/cat/uab/tqs/view/ConsoleGameView.java
+++ tqs/src/main/java/cat/uab/tqs/view/ConsoleGameView.java
@@ -4,6 +4,8 @@ import cat.uab.tqs.model.*;
 
 public class ConsoleGameView implements GameView {
 
+    private boolean dealerHidden = false;
+
     @Override
     public void showMessage(String msg) {
         System.out.println(msg);
@@ -11,11 +13,23 @@ public class ConsoleGameView implements GameView {
 
     @Override
     public void showCard(Player player, Card c) {
-        System.out.println("Card: " + c.getRank() + " of " + c.getSuit());
+        
+        System.out.println("Card drawn: " + c.getRank() + " of " + c.getSuit());
     }
 
     @Override
     public void updateScores(int playerValue, int dealerValue) {
-        System.out.println("Player: " + playerValue + ", Dealer: " + dealerValue);
+
+        if (dealerHidden) {
+            // si esta ocult no revelem la puntuacio del dealer
+            System.out.println("Player: " + playerValue + ", Dealer: [hidden]");
+        } else {
+            System.out.println("Player: " + playerValue + ", Dealer: " + dealerValue);
+        }
+    }
+
+    @Override
+    public void setDealerHidden(boolean hidden) {
+        this.dealerHidden = hidden;
     }
 }
