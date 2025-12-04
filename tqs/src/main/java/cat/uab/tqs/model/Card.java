a7f1472fd63eda94bb77cd259617fbb6a5b37599
arnaausole <1708094@uab.cat>
2025-12-04 09:59:30 +0100

    invariant + post/pre condicions classe card

--- tqs/src/main/java/cat/uab/tqs/model/Card.java
+++ tqs/src/main/java/cat/uab/tqs/model/Card.java
@@ -6,24 +6,52 @@ public class Card {
     private String suit;
     private String rank;
 
+    private boolean invariant() {
+
+        return suit != null && rank != null;
+    }
+
     public Card(String suit, String rank) {
+
+        //precondicions
+
+        assert suit != null;
+        assert rank != null;
+
         this.suit = suit;
         this.rank = rank;
+
+        assert invariant();
+
     }
 
     public String getSuit() {
+        assert invariant();
+
         return suit;
     }
 
     public String getRank() {
+
+        assert invariant();
+
         return rank;
     }
 
     public Integer getNumericValue() {
-        return switch (rank) {
+
+        assert invariant();
+
+        Integer value = switch (rank) {
             case "J", "Q", "K" -> 10;
             case "A" -> 11;
             default -> Integer.parseInt(rank);
         };
+
+        //postcondicio
+        assert value != null;
+        assert invariant();
+        return value;
+
     }
 }
