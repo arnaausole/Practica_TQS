3aed7ddbf7437a171e852afd85982ccd5bfaf5f8
arnaausole <1708094@uab.cat>
2025-12-04 10:32:27 +0100

    invaraint + pre/post cond dealer

--- tqs/src/main/java/cat/uab/tqs/model/Dealer.java
+++ tqs/src/main/java/cat/uab/tqs/model/Dealer.java
@@ -4,26 +4,53 @@ public class Dealer {
 
     private Hand hand;
 
+    private boolean invariant() {
+        return hand != null;
+    }
+
+
     public Dealer() {
         hand = new Hand();
+
+        assert invariant();
     }
 
     public Hand getHand() {
+        assert invariant();
         return hand;
     }
 
     public void play(Deck deck) {
+
+         assert invariant();
+         //precond
+         assert deck != null;
+
 
         while (hand.getValue() < 17 && !deck.isEmpty()) {
 
             Card c = deck.drawCard();
             hand.addCard(c);
         }
+
+        //postcond
+        assert hand.getValue() >= 17 || deck.isEmpty();
+        assert invariant();
         
     }
 
     public void reset() {
+
+        assert invariant();
+
+
         hand = new Hand();
+
+        assert invariant();
+
+        //postcond
+        assert hand.getCards().isEmpty();
+
     }
 
 
