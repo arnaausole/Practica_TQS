540ffb6ecc8434d0b2f23c10055b742289b6dd7d
arnaausole <1708094@uab.cat>
2025-12-04 12:13:18 +0100

    afegir casos de test

--- tqs/src/test/java/cat/uab/tqs/model/HandTest.java
+++ tqs/src/test/java/cat/uab/tqs/model/HandTest.java
@@ -100,6 +100,33 @@ public class HandTest {
         // Cas extra: mà buida
         hand = new Hand();
         assertEquals(0, hand.getValue());
+
+        // Decision Coverage: Cobrir el while (total > 21 && aces > 0)
+
+        // Cas 1: el while no entra (total ≤ 21)
+        hand = new Hand();
+        hand.addCard(new Card("Hearts", "A"));  
+        hand.addCard(new Card("Clubs", "5")); // = 16
+        assertEquals(16, hand.getValue());
+
+        // Cas 2: el while entra (total > 21 amb as)
+        hand = new Hand();
+        hand.addCard(new Card("Hearts", "A"));
+        hand.addCard(new Card("Clubs", "K"));
+        hand.addCard(new Card("Spades", "9"));  // = 30 --> ajusta = 20
+        assertEquals(20, hand.getValue());
+
+        // Condition Coverage --> cobrir if(c.getRank().equals("A"))
+
+        // Cas 1: true (es un as)
+        hand = new Hand();
+        hand.addCard(new Card("Hearts", "A")); // true
+        assertEquals(11, hand.getValue());
+
+        // Cas 2: false (no es un as)
+        hand = new Hand();
+        hand.addCard(new Card("Clubs", "9")); // false
+        assertEquals(9, hand.getValue());
     }
 
     @Test
@@ -133,26 +160,36 @@ public class HandTest {
     void testIsBlackjack() {
         Hand hand = new Hand();
 
-        // Cas blackjack natural
+        //condition coverage
+
+        // Cas blackjack natural: true/true
         hand.addCard(new Card("Hearts", "A"));
         hand.addCard(new Card("Spades", "K"));
         assertTrue(hand.isBlackjack());
 
-        // 2 cartes però no 21
+        // 2 cartes però no 21: true/false
         hand = new Hand();
         hand.addCard(new Card("Hearts", "10"));
         hand.addCard(new Card("Clubs", "9"));
         assertFalse(hand.isBlackjack());
 
-        // Més de 2 cartes
+        // Més de 2 cartes: false/true
         hand.addCard(new Card("Clubs", "2"));
         assertFalse(hand.isBlackjack());
 
-        // 21 amb 3 cartes
+        // 21 amb 3 cartes: false/true
         hand = new Hand();
         hand.addCard(new Card("Hearts", "7"));
         hand.addCard(new Card("Clubs", "7"));
         hand.addCard(new Card("Spades", "7"));
         assertFalse(hand.isBlackjack());
+
+        // no es 21 ni te 2 cartes: false/false
+        hand = new Hand();
+        hand.addCard(new Card("Hearts", "5"));
+        hand.addCard(new Card("Clubs", "5"));
+        hand.addCard(new Card("Spades", "5")); // !=2 cartes, !=21
+        assertFalse(hand.isBlackjack());
+
     }
 }
